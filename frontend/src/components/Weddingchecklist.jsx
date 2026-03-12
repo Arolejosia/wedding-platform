e47467d35bd66cbd19229a3faf741c2438f8a359
Arolejosia <92277980+Arolejosia@users.noreply.github.com>
2026-03-12 01:52:59 -0400

    Fix TypeScript build errors

--- frontend/src/components/Weddingchecklist.jsx
+++ frontend/src/components/Weddingchecklist.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './WeddingChecklist.css';
+import './Weddingchecklist.css';
 import { useParams } from "react-router-dom";
 import API_URL from '../config/api';
 
